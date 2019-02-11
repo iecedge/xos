1d03b84fc117937216f0ddc07882855fe6a18bf3
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-02-11 13:57:49 -0400

    Locally tag current version as manifest

--- ci_scripts/push_containers.sh
+++ ci_scripts/push_containers.sh
@@ -1,23 +1,23 @@
 export IMAGE_TAG=$(cat VERSION)
 export AARCH=`uname -m`
 cd containers/xos
-docker build -f Dockerfile.base -t cachengo/xos-base:master .
+docker build -f Dockerfile.base -t cachengo/xos-base:$IMAGE_TAG .
 cd ../..
-docker build -f containers/xos/Dockerfile.libraries -t cachengo/xos-libraries:master .
-docker build -f containers/xos/Dockerfile.xos-core -t cachengo/xos-core:master .
+docker build -f containers/xos/Dockerfile.libraries -t cachengo/xos-libraries:$IMAGE_TAG .
+docker build -f containers/xos/Dockerfile.xos-core -t cachengo/xos-core:$IMAGE_TAG .
 git clone https://github.com/opencord/chameleon.git -b master ./tmp.chameleon
-docker build -f containers/chameleon/Dockerfile.chameleon -t cachengo/chameleon:master .
+docker build -f containers/chameleon/Dockerfile.chameleon -t cachengo/chameleon:$IMAGE_TAG .
 rm -r tmp.chameleon
 git clone https://github.com/opencord/chameleon.git -b master ./containers/xos/tmp.chameleon
-docker build -f containers/xos/Dockerfile.client -t cachengo/xos-client:master .
-docker build -f containers/xos/Dockerfile.synchronizer-base -t cachengo/xos-synchronizer-base:master .
+docker build -f containers/xos/Dockerfile.client -t cachengo/xos-client:$IMAGE_TAG .
+docker build -f containers/xos/Dockerfile.synchronizer-base -t cachengo/xos-synchronizer-base:$IMAGE_TAG .
 
-docker tag cachengo/xos-base:master cachengo/xos-base-$AARCH:$IMAGE_TAG
-docker tag cachengo/xos-core:master cachengo/xos-core-$AARCH:$IMAGE_TAG
-docker tag cachengo/xos-libraries:master cachengo/xos-libraries-$AARCH:$IMAGE_TAG
-docker tag cachengo/chameleon:master cachengo/chameleon-$AARCH:$IMAGE_TAG
-docker tag cachengo/xos-client:master cachengo/xos-client-$AARCH:$IMAGE_TAG
-docker tag cachengo/xos-synchronizer-base:master cachengo/xos-synchronizer-base-$AARCH:$IMAGE_TAG
+docker tag cachengo/xos-base:$IMAGE_TAG cachengo/xos-base-$AARCH:$IMAGE_TAG
+docker tag cachengo/xos-core:$IMAGE_TAG cachengo/xos-core-$AARCH:$IMAGE_TAG
+docker tag cachengo/xos-libraries:$IMAGE_TAG cachengo/xos-libraries-$AARCH:$IMAGE_TAG
+docker tag cachengo/chameleon:$IMAGE_TAG cachengo/chameleon-$AARCH:$IMAGE_TAG
+docker tag cachengo/xos-client:$IMAGE_TAG cachengo/xos-client-$AARCH:$IMAGE_TAG
+docker tag cachengo/xos-synchronizer-base:$IMAGE_TAG cachengo/xos-synchronizer-base-$AARCH:$IMAGE_TAG
 
 docker push cachengo/xos-base-$AARCH:$IMAGE_TAG
 docker push cachengo/xos-core-$AARCH:$IMAGE_TAG
